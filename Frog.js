fecfd32ea1507b96801dc68ea723301c87d79984
PMH21 <42618173+PMH21@users.noreply.github.com>
2018-08-23 10:15:46 -0400

    Add files via upload

---
+++ Frog.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(800, 400);
+  background(204);
+}
+
+function draw() {
+  ellipse (115, 65, 85, 40)
+  ellipse (115, 70, 40, 10)
+  ellipse (135, 45, 10, 10)
+  ellipse (95, 45, 10, 10)
+}
